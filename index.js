9852b3832bb1bc3894d0931c636a9511181ea87a
nferdous2 <noorjahanferdous029@gmail.com>
2024-03-08 11:38:35 +0600

    edit

--- index.js
+++ index.js
@@ -303,8 +303,8 @@ async function run() {
         total_amount: order.totalPrice,
         currency: 'BDT',
         tran_id: tran_id, // use unique tran_id for each api call
-        success_url: `https://nature-nexus-backend.vercel.app/payment/success/${tran_id}`,
-        fail_url: `https://nature-nexus-backend.vercel.app/payment/fail/${tran_id}`,
+        success_url: `https://nature-nexus-backend-sable.vercel.app/payment/success/${tran_id}`,
+        fail_url: `https://nature-nexus-backend-sable.vercel.app/payment/fail/${tran_id}`,
         cancel_url: 'http://localhost:3030/cancel',
         ipn_url: 'http://localhost:3030/ipn',
         shipping_method: 'Courier',
